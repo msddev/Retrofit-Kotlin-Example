19fa80e00811e429f9d1e7dfbed51e1dfb992794
mkdev <Mk2580@@>
2021-08-21 03:13:35 +0430

    UpdatedAdded koin module

--- app/src/main/java/com/mkdev/retrofitpart/di/KoinModule.kt
+++ app/src/main/java/com/mkdev/retrofitpart/di/KoinModule.kt
@@ -1,7 +1,14 @@
 package com.mkdev.retrofitpart.di
 
-import org.koin.dsl.module.module
+import com.mkdev.retrofitpart.repository.ApiRepository
+import com.mkdev.retrofitpart.viewmodel.MainActivityViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
 
 val appModule = module {
+
+    single { ApiRepository() }
+
+    viewModel{ MainActivityViewModel(get()) }
 
 }
